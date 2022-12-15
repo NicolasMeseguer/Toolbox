2f473e1e1ab7ec1bdfd03790c65324d449fe654c
NicolasMeseguer <nicolasmeseguer@icloud.com>
2022-12-15 14:36:30 +0000

    Actualizado el codigo con comentarios.

--- AoC22/day1/main.c
+++ AoC22/day1/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define TOP 3
+#define TOP 5
 
 void initVector(int storeCals[], int size) {
 	
@@ -12,7 +12,7 @@ void initVector(int storeCals[], int size) {
 
 }
 
-void printStoreCals(int storeCals[], int size) {
+void printStoredCals(int storeCals[], int size) {
 	
 	for (int i = 0; i < size; ++i) {
 		printf("%i\n", storeCals[i]);
@@ -20,13 +20,13 @@ void printStoreCals(int storeCals[], int size) {
 
 }
 
-int printAccum(int storeCals[], int size) {
+void printAccum(int storeCals[], int size) {
 	int maxTOPAccum = 0;
 	for (int i = 0; i < size; ++i) {
 		maxTOPAccum += storeCals[i];
 	}
 
-	return maxTOPAccum;
+	printf("La suma de los %i elfos con mayor numero de calorias es: %i\n", size, maxTOPAccum);
 
 }
 
@@ -39,21 +39,24 @@ int main( int argc, char *argv[] ) {
 
 	// AoC Variables
 	int storeCals[TOP];
-
 	initVector(storeCals, TOP);
-	//printStoreCals(storeCals, TOP);
 	
+	// Do it as many times as elves'calories you want to retrieve
 	for(int i = 0; i < TOP; ++i) {
 		int cals = 0;
 		int maxCals = 0;
+
 		while(fgets(buffer, 255, ptr)) {
 			if(strcmp(buffer, "\n") == 0) {
 				int storeFlag = 1;
 
+				// Compare if the cals (accumulated) are already stored in the vector
+				// in case this is the max, amount.
 				for(int j = 0; j < TOP; ++j)
 					if (cals == storeCals[j])
-						storeFlag = 0;
+						storeFlag = 0;	
 
+				// If not, then simply compare and swap the value if greater than.
 				if (storeFlag)
 					maxCals = (cals>maxCals)?cals:maxCals;
 
@@ -65,14 +68,13 @@ int main( int argc, char *argv[] ) {
 
 		storeCals[i] = maxCals;
 
-
 		rewind(ptr);
 	}
 
 	fclose(ptr);
 
-	printStoreCals(storeCals, TOP);
-	printf("La suma de los %i elfos con mayor numero de calorias es: %i\n", TOP, printAccum(storeCals, TOP));
+	printStoredCals(storeCals, TOP);
+	printAccum(storeCals, TOP);
 
 	return 0;
 }
